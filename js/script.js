000518546d2ce11e307c070900808a6a81be4f8c
Anatoliy <davidovhz@yandex.ru>
2023-11-09 14:05:30 +0400

    create block-generation, company, company_two, hulot

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let ibgs = document.querySelectorAll('.ibg');
 for (let ibg of ibgs) {
     let img = ibg.querySelector('img');
+    console.log(img);
     if (img) {
          let src=img.getAttribute('src');
           ibg.style.backgroundImage = `url(${src})`;
@@ -14,3 +15,20 @@ burger.addEventListener('click', function () {
     menuMainBlock.classList.toggle('activve');
     document.body.classList.toggle('lock');
 })
+
+let mainBlock__image = document.querySelector('.mainBlock__image');
+let li_logo=document.querySelector('.li-logo');
+function pow() {
+    let i =0- scrollY/2;
+    mainBlock__image.style.top = i + 'px';
+    
+    if (window.innerWidth < 821) {
+        if (window.scrollY < 1) {
+            li_logo.style.top = '8px';
+            return;
+        }
+       li_logo.style.top= i + 'px';
+    }
+}
+window.addEventListener('scroll', pow);
+pow();
